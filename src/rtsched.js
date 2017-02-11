1cf9ec6364518e2eb4d88f07fed6426d2e8a2776
Emilio Veloci <feedingaliencat@gmail.com>
2017-02-11 17:14:52 +0100

    Get data from form in js

--- src/rtsched.js
+++ src/rtsched.js
@@ -1,6 +1,17 @@
-console.log('js imported');
 
+function getData() {
+    var data = new Array();
 
-function manageForm() {
-    console.log('form!')
+    $('#input_data .data_field').each(function() {
+        var el = {
+            priority:$( this ).find( 'input.priority' ).val(),
+            period:$( this ).find( 'input.period' ).val(),
+            wcet:$( this ).find( 'input.wcet' ).val(),
+        };
+
+        if ((el.priority || el.priority == "0") && el.period && el.wcet) {
+            data.push(el);
+        }
+    });
+
 }
